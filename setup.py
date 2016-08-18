0084bceebdeab39884e8771aa0d52b86f30bc91d
Haochi Chen <haochi.chen@gmail.com>
2016-08-18 10:38:47 -0300

    Set up PyPi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'personalcapital',
+  packages = ['personalcapital'], # this must be the same as the name above
+  version = '1.0.0',
+  description = 'Personal Capital library for accessing its API',
+  author = 'Haochi Chen',
+  author_email = 'pypi@ihaochi.com',
+  url = 'https://github.com/haochi/personalcapital',
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/1.0.0',
+  keywords = ['personal capital', 'financial', 'money'],
+  classifiers = [],
+)
