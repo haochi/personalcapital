5cb4098d1f4ac6d0beb404d0528c213d63e89f0a
Haochi Chen <haochi.chen@gmail.com>
2016-08-18 10:45:01 -0300

    Fix download url

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from distutils.core import setup
+version = '1.0.1'
+
 setup(
   name = 'personalcapital',
   packages = ['personalcapital'], # this must be the same as the name above
-  version = '1.0.0',
+  version = version,
   description = 'Personal Capital library for accessing its API',
   author = 'Haochi Chen',
   author_email = 'pypi@ihaochi.com',
   url = 'https://github.com/haochi/personalcapital',
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/1.0.0',
+  download_url = 'https://github.com/haochi/personalcapital/tarball/{0}'.format(version),
   keywords = ['personal capital', 'financial', 'money'],
   classifiers = [],
 )
